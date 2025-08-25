a178f4cc3991c6c174234f356d3145c755cf0981
Yuri Boczar <123526190+B0czar@users.noreply.github.com>
2025-08-24 22:35:06 -0300

    Add validation and calculations to the calculate function in mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,46 @@
 import numpy as np
 
-def calculate(list):
+def calculate(lst):
+    if len(lst) != 9:
+        raise ValueError("List must contain nine numbers.")
 
+    
+    arr = np.array(lst).reshape(3, 3)
 
+    def to_list(x):
+        return [i.item() if hasattr(i, "item") else i for i in list(x)]
 
+    calculations = {
+        'mean': [
+            to_list(np.mean(arr, axis=0)),     
+            to_list(np.mean(arr, axis=1)),     
+            np.mean(arr).item()                
+        ],
+        'variance': [
+            to_list(np.var(arr, axis=0)),
+            to_list(np.var(arr, axis=1)),
+            np.var(arr).item()
+        ],
+        'standard deviation': [
+            to_list(np.std(arr, axis=0)),
+            to_list(np.std(arr, axis=1)),
+            np.std(arr).item()
+        ],
+        'max': [
+            to_list(np.max(arr, axis=0)),
+            to_list(np.max(arr, axis=1)),
+            int(np.max(arr))
+        ],
+        'min': [
+            to_list(np.min(arr, axis=0)),
+            to_list(np.min(arr, axis=1)),
+            int(np.min(arr))
+        ],
+        'sum': [
+            to_list(np.sum(arr, axis=0)),
+            to_list(np.sum(arr, axis=1)),
+            int(np.sum(arr))
+        ]
+    }
 
     return calculations
